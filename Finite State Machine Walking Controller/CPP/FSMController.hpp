03fd63b8bebd3d38c903c1601fc92b821f5f3e36
jderosia <134736683+jderosia@users.noreply.github.com>
2023-10-24 17:40:15 -0400

    Fixed FSMController.hpp

--- Finite State Machine Walking Controller/CPP/FSMController.hpp
+++ Finite State Machine Walking Controller/CPP/FSMController.hpp
@@ -1,6 +1,4 @@
 
 // FSMController function prototype
-void FSMController(Hardware *hardware, const FSMParameters *params, 
-                   State *currentStateOut, double *kneeTheta, 
-                   double *kneeK, double *kneeB, double *ankleTheta, 
-                   double *ankleK, double *ankleB);
+
+FSM_Outputs FSMController(FSM_Inputs);
