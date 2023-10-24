3f82a168ecf23f0b9fc0331056e9c54294055c90
jderosia <134736683+jderosia@users.noreply.github.com>
2023-10-24 17:39:54 -0400

    Fixed FSMController.cpp

--- Finite State Machine Walking Controller/CPP/FSMController.cpp
+++ Finite State Machine Walking Controller/CPP/FSMController.cpp
@@ -5,171 +5,125 @@
 
 /**
  * This function implements a finite state machine (FSM) controller for the
- * OSL. The FSM has four states: ESTANCE, LSTANCE, ESWING, and LSWING. The
- * FSM transitions between states based on the current state, the current
- * time in state, and the current hardwarePtr readings. The FSM outputs the
- * desired knee and ankle  equilibrium angles and gains for the current state.
- * 
- * @param currentTimeInStatePtr Pointer to the current time in state
- * @param hardwarePtr Pointer to the hardwarePtr struct which has a knee joint, ankle joint and loadcell struct as attributes
- * @param paramsPtr Pointer to the paramsPtr struct which has a body weight and four structs for the four states as attributes
- * @param currentStateOutPtr Pointer to the current state
- * @param kneeThetaPtr Pointer to the desired knee equilibrium angle
- * @param kneeKPtr Pointer to the desired knee gain
- * @param kneeBPtr Pointer to the desired knee damping
- * @param ankleThetaPtr Pointer to the desired ankle equilibrium angle
- * @param ankleKPtr Pointer to the desired ankle gain
- * @param ankleBPtr Pointer to the desired ankle damping
- * 
- * @return void
- * 
- * @see FSMStructs.hpp
- * 
- * @note See FSMController_tb.cpp to see how this function is called
- * @note This function modifies the values of the pointers passed to it
+ * OSL. The FSM has four states: ESTANCE, LSTANCE, ESWING, and LSWING. FINISH!
  * 
 */
-void FSMController(Hardware *hardwarePtr,
-                   const FSMParameters *paramsPtr, State *currentStateOutPtr,
-                   double *kneeThetaPtr, double *kneeKPtr, double *kneeBPtr,
-                   double *ankleThetaPtr, double *ankleKPtr, double *ankleBPtr) {
+FSM_Outputs FSMController(FSM_Inputs inputs) {
 
     // If this is the first function call, initialize the current state to ESTANCE
+    Sensors sensors = inputs.sensors;
+    FSMParameters params = inputs.parameters;
+    
     static State currentState = ESTANCE;
+    static double currentTimeInState = 0.0;
+    static double time_last;
+
+    double dt = inputs.time - time_last;
+    time_last = inputs.time;
 
     // When in ESTANCE
     if (currentState == ESTANCE) {
         
         // Conditions to transition to LSTANCE
-        if ((hardwarePtr->osl.loadcell.fz > paramsPtr->eStanceParams.loadLStance) &&
-        (hardwarePtr->osl.ankle.outputPosition > paramsPtr->eStanceParams.ankleThetaEStanceToLStance) &&
-        (hardwarePtr->currentTimeInState > paramsPtr->minTimeInState)) {
+        if ((sensors.Fz < params.transitionParameters.loadLStance) &&
+            (sensors.ankleAngle > params.transitionParameters.ankleThetaEStanceToLStance) &&
+            (currentTimeInState > params.transitionParameters.minTimeInState)) {
+                // Update to LSTANCE
+                currentState = LSTANCE;
+                currentTimeInState = 0.0;
 
-            // Update to LSTANCE
-            currentState = LSTANCE;
-            *kneeThetaPtr = paramsPtr->lStanceParams.kneeTheta;
-            *kneeKPtr = paramsPtr->lStanceParams.kneeK;
-            *kneeBPtr = paramsPtr->lStanceParams.kneeB;
-            *ankleThetaPtr = paramsPtr->lStanceParams.ankleTheta;
-            *ankleKPtr = paramsPtr->lStanceParams.ankleK;
-            *ankleBPtr = paramsPtr->lStanceParams.ankleB;
-            hardwarePtr->currentTimeInState = 0.0;
-
-        // Otherwise, remain in LSTANCE
+        // Otherwise, remain in ESTANCE
         } else {
             currentState = ESTANCE;
-            *kneeThetaPtr = paramsPtr->eStanceParams.kneeTheta;
-            *kneeKPtr = paramsPtr->eStanceParams.kneeK;
-            *kneeBPtr = paramsPtr->eStanceParams.kneeB;
-            *ankleThetaPtr = paramsPtr->eStanceParams.ankleTheta;
-            *ankleKPtr = paramsPtr->eStanceParams.ankleK;
-            *ankleBPtr = paramsPtr->eStanceParams.ankleB;
-            hardwarePtr->currentTimeInState += hardwarePtr->time;
+            currentTimeInState += dt;
         }
     
     // When in LSTANCE
     } else if (currentState == LSTANCE) {
         
         // Conditions to transition to ESWING
-        if ((hardwarePtr->osl.loadcell.fz > paramsPtr->lStanceParams.loadESwing) &&
-        (hardwarePtr->currentTimeInState > paramsPtr->minTimeInState)) {
+        if ((sensors.Fz > params.transitionParameters.loadLStance) &&
+            (currentTimeInState > params.transitionParameters.minTimeInState)) {
+                // Update to ESWING
+                currentState = ESWING;
+                currentTimeInState = 0.0;
             
-            // Update to ESWING
-            currentState = ESWING;
-            *kneeThetaPtr = paramsPtr->eSwingParams.kneeTheta;
-            *kneeKPtr = paramsPtr->eSwingParams.kneeK;
-            *kneeBPtr = paramsPtr->eSwingParams.kneeB;
-            *ankleThetaPtr = paramsPtr->eSwingParams.ankleTheta;
-            *ankleKPtr = paramsPtr->eSwingParams.ankleK;
-            *ankleBPtr = paramsPtr->eSwingParams.ankleB;
-            hardwarePtr->currentTimeInState = 0.0;
-        
         // Otherwise, remain in LSTANCE
         } else {
             currentState = LSTANCE;
-            *kneeThetaPtr = paramsPtr->lStanceParams.kneeTheta;
-            *kneeKPtr = paramsPtr->lStanceParams.kneeK;
-            *kneeBPtr = paramsPtr->lStanceParams.kneeB;
-            *ankleThetaPtr = paramsPtr->lStanceParams.ankleTheta;
-            *ankleKPtr = paramsPtr->lStanceParams.ankleK;
-            *ankleBPtr = paramsPtr->lStanceParams.ankleB;
-            hardwarePtr->currentTimeInState += hardwarePtr->time;
+            currentTimeInState += dt;
         }
+    
     // When in ESWING
     } else if (currentState == ESWING) {
 
         // Conditions to transition to LSWING
-        if ((hardwarePtr->osl.knee.outputPosition >
-        paramsPtr->eSwingParams.kneeThetaESwingToLSwing) &&
-        (hardwarePtr->osl.knee.outputVelocity <
-         paramsPtr->eSwingParams.kneeDThetaESwingToLSwing) &&
-        (hardwarePtr->currentTimeInState > paramsPtr->minTimeInState)) {
-
-            // Update to LSWING
-            currentState = LSWING;
-            *kneeThetaPtr = paramsPtr->lSwingParams.kneeTheta;
-            *kneeKPtr = paramsPtr->lSwingParams.kneeK;
-            *kneeBPtr = paramsPtr->lSwingParams.kneeB;
-            *ankleThetaPtr = paramsPtr->lSwingParams.ankleTheta;
-            *ankleKPtr = paramsPtr->lSwingParams.ankleK;
-            *ankleBPtr = paramsPtr->lSwingParams.ankleB;
-            hardwarePtr->currentTimeInState = 0.0;
+        if ((sensors.kneeAngle > params.transitionParameters.kneeThetaESwingToLSwing) &&
+            (sensors.kneeVelocity < params.transitionParameters.kneeDThetaESwingToLSwing) &&
+            (currentTimeInState > params.transitionParameters.minTimeInState)) {
+                // Update to LSWING
+                currentState = LSWING;
+                currentTimeInState = 0.0;
         
         // Otherwise, remain in ESWING
         } else {
             currentState = ESWING;
-            *kneeThetaPtr = paramsPtr->eSwingParams.kneeTheta;
-            *kneeKPtr = paramsPtr->eSwingParams.kneeK;
-            *kneeBPtr = paramsPtr->eSwingParams.kneeB;
-            *ankleThetaPtr = paramsPtr->eSwingParams.ankleTheta;
-            *ankleKPtr = paramsPtr->eSwingParams.ankleK;
-            *ankleBPtr = paramsPtr->eSwingParams.ankleB;
-            hardwarePtr->currentTimeInState += hardwarePtr->time;
+            currentTimeInState += dt;
         }
 
     // When in LSWING
     } else if (currentState == LSWING) {
 
         // Conditions to transition to ESTANCE
-        if ((hardwarePtr->osl.loadcell.fz < paramsPtr->lSwingParams.loadEStance) ||
-        (hardwarePtr->osl.knee.outputPosition <
-        paramsPtr->lSwingParams.kneeThetaLSwingToEStance)) {
-            
-            // Update to ESTANCE
-            currentState = ESTANCE;
-            *kneeThetaPtr = paramsPtr->eStanceParams.kneeTheta;
-            *kneeKPtr = paramsPtr->eStanceParams.kneeK;
-            *kneeBPtr = paramsPtr->eStanceParams.kneeB;
-            *ankleThetaPtr = paramsPtr->eStanceParams.ankleTheta;
-            *ankleKPtr = paramsPtr->eStanceParams.ankleK;
-            *ankleBPtr = paramsPtr->eStanceParams.ankleB;
-            hardwarePtr->currentTimeInState = 0.0;
+        if ((sensors.Fz < params.transitionParameters.loadEStance) &&
+            (sensors.kneeAngle < params.transitionParameters.kneeThetaLSwingToEStance) &&
+            (currentTimeInState > params.transitionParameters.minTimeInState)) {
+                // Update to ESTANCE
+                currentState = ESTANCE;
+                currentTimeInState = 0.0;
         
         // Otherwise, remain in LSWING
         } else {
             currentState = LSWING;
-            *kneeThetaPtr = paramsPtr->lSwingParams.kneeTheta;
-            *kneeKPtr = paramsPtr->lSwingParams.kneeK;
-            *kneeBPtr = paramsPtr->lSwingParams.kneeB;
-            *ankleThetaPtr = paramsPtr->lSwingParams.ankleTheta;
-            *ankleKPtr = paramsPtr->lSwingParams.ankleK;
-            *ankleBPtr = paramsPtr->lSwingParams.ankleB;
-            hardwarePtr->currentTimeInState += hardwarePtr->time;
+            currentTimeInState += dt;
         }
 
     // If the current state is not one of the four states, set the current state to ESTANCE
     } else {
         currentState = ESTANCE;
-        *kneeThetaPtr = paramsPtr->eStanceParams.kneeTheta;
-        *kneeKPtr = paramsPtr->eStanceParams.kneeK;
-        *kneeBPtr = paramsPtr->eStanceParams.kneeB;
-        *ankleThetaPtr = paramsPtr->eStanceParams.ankleTheta;
-        *ankleKPtr = paramsPtr->eStanceParams.ankleK;
-        *ankleBPtr = paramsPtr->eStanceParams.ankleB;
-        hardwarePtr->currentTimeInState = 0.0;
+        currentTimeInState += dt;
     }
 
-    // Update the current state
-    *currentStateOutPtr = currentState;
+    // Select the impedance parameters for the current state
+    ImpedanceParameters kneeImpedance;
+    ImpedanceParameters ankleImpedance;
+
+    if (currentState == ESTANCE) {
+        kneeImpedance = params.kneeImpedance.earlyStance;
+        ankleImpedance = params.ankleImpedance.earlyStance;
+    } else if (currentState == LSTANCE) {
+        kneeImpedance = params.kneeImpedance.lateStance;
+        ankleImpedance = params.ankleImpedance.lateStance;
+    } else if (currentState == ESWING) {
+        kneeImpedance = params.kneeImpedance.earlySwing;
+        ankleImpedance = params.ankleImpedance.earlySwing;
+    } else if (currentState == LSWING) {
+        kneeImpedance = params.kneeImpedance.lateSwing;
+        ankleImpedance = params.ankleImpedance.lateSwing;
+    } else {
+        kneeImpedance = params.kneeImpedance.earlyStance;
+        ankleImpedance = params.ankleImpedance.earlyStance;
+    }
+
+
+    // Set outputs
+    FSM_Outputs outputs;
+
+    outputs.currentState = currentState;
+    outputs.timeInCurrentState = currentTimeInState;
+    outputs.kneeImpedance = kneeImpedance;
+    outputs.ankleImpedance = ankleImpedance;
+
+    return outputs;
 
 }
