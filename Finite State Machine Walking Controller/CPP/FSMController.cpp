1afbbd7dbb540d56408b841d6368199b6149058a
Kevin Best <tkevinbest@gmail.com>
2023-11-10 16:50:59 +0000

    General cleanup.

--- Finite State Machine Walking Controller/CPP/FSMController.cpp
+++ Finite State Machine Walking Controller/CPP/FSMController.cpp
@@ -2,8 +2,10 @@
 
 /**
  * This function implements a finite state machine (FSM) controller for the
- * OSL. The FSM has four states: ESTANCE, LSTANCE, ESWING, and LSWING. FINISH!
+ * OSL. The FSM has four states: ESTANCE, LSTANCE, ESWING, and LSWING. 
  * 
+ * Jace Derosia, Kevin Best
+ * October 2023
 */
 extern "C" void FSMController(FSM_Inputs *inputs, FSM_Outputs *outputs) {
 
@@ -12,6 +14,7 @@ extern "C" void FSMController(FSM_Inputs *inputs, FSM_Outputs *outputs) {
     FSMParameters params = inputs->parameters;
     
     static State currentState = ESTANCE;
+    static State previousState = ESTANCE;
     static double currentTimeInState = 0.0;
     static double time_last;
 
@@ -20,98 +23,95 @@ extern "C" void FSMController(FSM_Inputs *inputs, FSM_Outputs *outputs) {
 
     // When in ESTANCE
     if (currentState == ESTANCE) {
-        
         // Conditions to transition to LSTANCE
         if ((sensors.Fz < params.transitionParameters.loadLStance) &&
             (sensors.ankleAngle > params.transitionParameters.ankleThetaEStanceToLStance) &&
             (currentTimeInState > params.transitionParameters.minTimeInState)) {
                 // Update to LSTANCE
                 currentState = LSTANCE;
-                currentTimeInState = 0.0;
-
         // Otherwise, remain in ESTANCE
         } else {
             currentState = ESTANCE;
-            currentTimeInState += dt;
         }
     
     // When in LSTANCE
     } else if (currentState == LSTANCE) {
-        
         // Conditions to transition to ESWING
         if ((sensors.Fz > params.transitionParameters.loadLStance) &&
             (currentTimeInState > params.transitionParameters.minTimeInState)) {
                 // Update to ESWING
                 currentState = ESWING;
-                currentTimeInState = 0.0;
-            
         // Otherwise, remain in LSTANCE
         } else {
             currentState = LSTANCE;
-            currentTimeInState += dt;
         }
     
     // When in ESWING
     } else if (currentState == ESWING) {
-
         // Conditions to transition to LSWING
         if ((sensors.kneeAngle > params.transitionParameters.kneeThetaESwingToLSwing) &&
             (sensors.kneeVelocity < params.transitionParameters.kneeDThetaESwingToLSwing) &&
             (currentTimeInState > params.transitionParameters.minTimeInState)) {
                 // Update to LSWING
                 currentState = LSWING;
-                currentTimeInState = 0.0;
-        
         // Otherwise, remain in ESWING
         } else {
             currentState = ESWING;
-            currentTimeInState += dt;
         }
 
     // When in LSWING
     } else if (currentState == LSWING) {
-
         // Conditions to transition to ESTANCE
         if (((sensors.Fz < params.transitionParameters.loadEStance) ||
             (sensors.kneeAngle < params.transitionParameters.kneeThetaLSwingToEStance)) &&
             (currentTimeInState > params.transitionParameters.minTimeInState)) {
                 // Update to ESTANCE
-                currentState = ESTANCE;
-                currentTimeInState = 0.0;
-        
+                currentState = ESTANCE;        
         // Otherwise, remain in LSWING
         } else {
             currentState = LSWING;
-            currentTimeInState += dt;
         }
 
     // If the current state is not one of the four states, set the current state to ESTANCE
     } else {
         currentState = ESTANCE;
-        currentTimeInState += dt;
     }
 
     // Select the impedance parameters for the current state
     ImpedanceParameters kneeImpedance;
     ImpedanceParameters ankleImpedance;
 
-    if (currentState == ESTANCE) {
-        kneeImpedance = params.kneeImpedance.earlyStance;
-        ankleImpedance = params.ankleImpedance.earlyStance;
-    } else if (currentState == LSTANCE) {
-        kneeImpedance = params.kneeImpedance.lateStance;
-        ankleImpedance = params.ankleImpedance.lateStance;
-    } else if (currentState == ESWING) {
-        kneeImpedance = params.kneeImpedance.earlySwing;
-        ankleImpedance = params.ankleImpedance.earlySwing;
-    } else if (currentState == LSWING) {
-        kneeImpedance = params.kneeImpedance.lateSwing;
-        ankleImpedance = params.ankleImpedance.lateSwing;
-    } else {
-        kneeImpedance = params.kneeImpedance.earlyStance;
-        ankleImpedance = params.ankleImpedance.earlyStance;
+    switch(currentState) {
+        case ESTANCE:
+            kneeImpedance = params.kneeImpedance.earlyStance;
+            ankleImpedance = params.ankleImpedance.earlyStance;
+            break;
+        case LSTANCE:
+            kneeImpedance = params.kneeImpedance.lateStance;
+            ankleImpedance = params.ankleImpedance.lateStance;
+            break;
+        case ESWING:
+            kneeImpedance = params.kneeImpedance.earlySwing;
+            ankleImpedance = params.ankleImpedance.earlySwing;
+            break;
+        case LSWING:
+            kneeImpedance = params.kneeImpedance.lateSwing;
+            ankleImpedance = params.ankleImpedance.lateSwing;
+            break;
+        default:
+            kneeImpedance = params.kneeImpedance.earlyStance;
+            ankleImpedance = params.ankleImpedance.earlyStance;
     }
 
+    // Track time in state
+    if (currentState == previousState) {
+        currentTimeInState += dt;
+    } else {
+        currentTimeInState = 0.0;
+    }
+
+    // Update previous state
+    previousState = currentState;
 
     // Set outputs
     outputs->currentState = currentState;
